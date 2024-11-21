8c00dfc323b6e171e8a120939d1c6a126dc01ec6
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-11-21 16:20:26 -0300

    Images to public folder

--- src/components/service/index.jsx
+++ src/components/service/index.jsx
@@ -10,12 +10,12 @@ const Service = () => {
             <h1>Serviços</h1>
             <p>A Schiavi Biker House é uma oficina especializada em Harley-Davidson®. Realizamos revisões completas, remapeamento de motores e diagnósticos precisos, além de cuidar de suspensão, pneus e freios para garantir o máximo desempenho e segurança. Também oferecemos peças e acessórios oficiais da Harley-Davidson®, incluindo capacetes, jaquetas e muito mais.</p>
             <div id="backgrounddeco">
-                <img src="src/assets/sbhlogoblack.png" alt="Logo Schiavi Biker House" />
+                <img src="public/assets/sbhlogoblack.png" alt="Logo Schiavi Biker House" />
             </div>
             <div className="containers">
-                <Container url = 'src/assets/Manutencao.png'/>
-                <Container url = 'src/assets/Suspensao.png'/>
-                <Container url = 'src/assets/Dinamometro.png'/>
+                <Container url = 'public/assets/Manutencao.png'/>
+                <Container url = 'public/assets/Suspensao.png'/>
+                <Container url = 'public/assets/Dinamometro.png'/>
             </div>
         </div>
     </>
